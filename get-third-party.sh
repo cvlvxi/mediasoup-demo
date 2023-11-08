86a66cd678510067af885805be0821b07320a4eb
cvlvxi <cvlvxi@outlook.com>
2023-11-08 16:51:33 +1100

    add third-party references and fetcher

---
+++ get-third-party.sh
@@ -0,0 +1,6 @@
+# Python
+mkdir -p third-party/python
+
+git clone git@github.com:aiortc/aiortc.git third-party/python/aiortc
+git clone git@github.com:jimdragongod/mediasoup-client-pysdk.git third-party/python/mediasoup-client-pysdk
+git clone git@github.com:skymaze/pymediasoup.git third-party/python/pymediasoup
